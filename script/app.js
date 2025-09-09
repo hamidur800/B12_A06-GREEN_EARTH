731b80eaf9b5fa1ecf678dca72654a2b41325a07
hamidur800 <hamidur800t@gmail.com>
2025-09-09 07:57:20 +0600

    Alhamdullah_Fainaly_All_Done_Erorr-Khaita-Khaita-sesh

--- script/app.js
+++ script/app.js
@@ -1,7 +1,197 @@
+// const categoriesData = () => {
+//   fetch("https://openapi.programming-hero.com/api/categories")
+//     .then((res) => res.json())
+//     .then((deta) => displyCatData(deta.categories));
+// };
+
+// const displyCatData = (CatDatas) => {
+//   const categoriesLevel = document.getElementById("categories");
+//   categoriesLevel.innerHTML = "";
+
+//   for (let CatData of CatDatas) {
+//     // console.log(CatData);
+//     const CatBtn = document.createElement("div");
+//     CatBtn.innerHTML = `
+//    <li class="space-y-4 hover:bg-lime-400 cursor-pointer rounded-sm  p-2 transition "> <button onclick="InnrCat(${CatData.id})" class="class="w-full text-left px-3 py-1 rounded bg-green-600 text-white ">${CatData.category_name}</button></li>
+//     `;
+
+//     categoriesLevel.appendChild(CatBtn);
+//   }
+// };
+
+// categoriesData();
+
+// const InnrCat = (id) => {
+//   // console.log(id);
+
+//   const url = `https://openapi.programming-hero.com/api/category/${id}`;
+//   fetch(url)
+//     .then((res) => res.json())
+//     .then((data) => displyInnrCat(data.plants));
+// };
+
+// const displyInnrCat = (InnrCats) => {
+//   // console.log(InnrCats);
+//   const categoriesContainer = document.getElementById("allplants");
+//   categoriesContainer.innerHTML = "";
+
+//   for (let InnrCat of InnrCats) {
+//     // console.log(InnrCat);
+//     const categoriCard = document.createElement("div");
+//     categoriCard.innerHTML = `
+//       <div class="bg-white h-full rounded-xl shadow  hover:shadow-lg transition">
+//                                 <div class=" bg-gray-200 rounded flex items-center justify-center">
+//                                     <img loading="lazy" class="w-full h-64 object-cover rounded-t-lg" src="${InnrCat.image}" alt="">
+//                                 </div>
+//                                 <button class="font-semibold px-3 mt-2">${InnrCat.name}</button>
+
+//                                 <p class="text-sm p-3 text-gray-600">${InnrCat.description}</p>
+//                                 <div class="flex justify-between items-center px-3 mt-2">
+//                                     <span
+//                                         class="text-xs  px-3 py-1 bg-green-100 text-green-700 rounded">${InnrCat.category}</span>
+//                                     <span class="font-semibold">${InnrCat.price}</span>
+//                                 </div>
+//                                 <div class="p-3">
+//                                     <button
+//                                         class="addBtn w-full mt-3 mx-auto py-2 rounded-full bg-green-600 text-white hover:bg-green-700">
+//                                         Add to Cart</button>
+//                                 </div>
+//                             </div>
+//     `;
+
+//     const addBtn = categoriCard.querySelector(".addBtn");
+//     addBtn.addEventListener("click", () => {
+//       alert(`${InnrCat.name} has been added to the card`);
+//       const cartDisplay = document.getElementById("cartsBtn");
+//       const cartContainer = document.createElement("div");
+//       cartContainer.classList.add(
+//         "flex",
+//         "items-center",
+//         "justify-between",
+//         "p-2"
+//       );
+
+//       cartContainer.innerHTML = `
+//         <div>
+//           <h2 class="font-semibold">${InnrCat.name}</h2>
+//           <p class="text-sm text-gray-600">৳${InnrCat.price}</p>
+//         </div>
+//         <div>
+//           <i class="fa-solid fa-x cursor-pointer text-red-500 removeBtn"></i>
+//         </div>
+//       `;
+
+//       cartDisplay.appendChild(cartContainer);
+
+//       total += parseFloat(InnrCat.price);
+//       document.getElementById("totalPrice").innerText = total;
+
+//       const removeBtn = cartContainer.querySelector(".removeBtn");
+//       removeBtn.addEventListener("click", () => {
+//         cartContainer.remove();
+
+//         total -= parseFloat(InnrCat.price);
+//         document.getElementById("totalPrice").innerText = total;
+//       });
+//     });
+
+//     categoriesContainer.appendChild(categoriCard);
+//   }
+// };
+
+// let total = 0;
+
+// const plantsData = () => {
+//   fetch("https://openapi.programming-hero.com/api/plants")
+//     .then((res) => res.json())
+//     .then((data) => allPlantsDisply(data.plants));
+// };
+
+// const allPlantsDisply = (allPlants) => {
+//   const allPlantslevel = document.getElementById("allplants");
+//   allPlantslevel.innerHTML = "";
+
+//   for (let allPlant of allPlants) {
+//     const plantsCard = document.createElement("div");
+//     plantsCard.innerHTML = `
+//       <div class="bg-white h-full rounded-xl shadow hover:shadow-lg transition">
+//         <div class="bg-gray-200 rounded flex items-center justify-center">
+//           <img loading="lazy" class="w-full h-64 object-cover rounded-t-lg" src="${allPlant.image}" alt="">
+//         </div>
+//         <button class="openModal font-semibold px-3 mt-2">${allPlant.name}</button>
+//         <p class="text-sm p-3 text-gray-600">${allPlant.description}</p>
+//         <div class="flex justify-between items-center px-3 mt-2">
+//           <span class="text-xs px-3 py-1 bg-green-100 text-green-700 rounded">${allPlant.category}</span>
+//           <span class="font-semibold">৳${allPlant.price}</span>
+//         </div>
+//         <div class="p-3">
+//           <button
+//             class="addBtn w-full mt-3 mx-auto py-2 rounded-full bg-green-600 text-white hover:bg-green-700">
+//             Add to Cart
+//           </button>
+//         </div>
+//       </div>
+//     `;
+
+//     const addBtn = plantsCard.querySelector(".addBtn");
+//     addBtn.addEventListener("click", () => {
+//       alert(`${allPlant.name} has been added to the card`);
+//       const cartDisplay = document.getElementById("cartsBtn");
+//       const cartContainer = document.createElement("div");
+//       cartContainer.classList.add(
+//         "flex",
+//         "items-center",
+//         "justify-between",
+//         "p-2"
+//       );
+
+//       cartContainer.innerHTML = `
+//         <div>
+//           <h2 class="font-semibold">${allPlant.name}</h2>
+//           <p class="text-sm text-gray-600">৳${allPlant.price}</p>
+//         </div>
+//         <div>
+//           <i class="fa-solid fa-x cursor-pointer text-red-500 removeBtn"></i>
+//         </div>
+//       `;
+
+//       cartDisplay.appendChild(cartContainer);
+
+//       total += parseFloat(allPlant.price);
+//       document.getElementById("totalPrice").innerText = total;
+
+//       const removeBtn = cartContainer.querySelector(".removeBtn");
+//       removeBtn.addEventListener("click", () => {
+//         cartContainer.remove();
+
+//         total -= parseFloat(allPlant.price);
+//         document.getElementById("totalPrice").innerText = total;
+//       });
+//     });
+
+//     allPlantslevel.appendChild(plantsCard);
+//   }
+// };
+
+// plantsData();
+
+let total = 0;
+
+function showLoader() {
+  document.getElementById("loader").classList.remove("hidden");
+}
+function hideLoader() {
+  document.getElementById("loader").classList.add("hidden");
+}
+
 const categoriesData = () => {
+  showLoader();
   fetch("https://openapi.programming-hero.com/api/categories")
     .then((res) => res.json())
-    .then((deta) => displyCatData(deta.categories));
+    .then((deta) => {
+      displyCatData(deta.categories);
+      hideLoader();
+    });
 };
 
 const displyCatData = (CatDatas) => {
@@ -9,10 +199,14 @@ const displyCatData = (CatDatas) => {
   categoriesLevel.innerHTML = "";
 
   for (let CatData of CatDatas) {
-    // console.log(CatData);
-    const CatBtn = document.createElement("div");
+    const CatBtn = document.createElement("li");
+    CatBtn.classList.add("space-y-4", "cursor-pointer");
+
     CatBtn.innerHTML = `
-   <li class="space-y-4 hover:bg-lime-400 cursor-pointer rounded-sm  p-2 transition "> <button onclick="InnrCat(${CatData.id})" class="class="w-full text-left px-3 py-1 rounded bg-green-600 text-white ">${CatData.category_name}</button></li>
+      <button onclick="InnrCat(${CatData.id}, this)" 
+        class="w-full text-left px-3 py-1 rounded hover:bg-[#15803d] hover:text-white cursor-pointer">
+        ${CatData.category_name}
+      </button>
     `;
 
     categoriesLevel.appendChild(CatBtn);
@@ -21,53 +215,153 @@ const displyCatData = (CatDatas) => {
 
 categoriesData();
 
-const InnrCat = (id) => {
-  // console.log(id);
+const InnrCat = (id, btn) => {
+  const allBtns = document.querySelectorAll("#categories button");
+  allBtns.forEach((b) => b.classList.remove("bg-green-600", "text-white"));
 
+  if (btn) {
+    btn.classList.add("bg-green-600", "text-white");
+  }
+
+  showLoader();
   const url = `https://openapi.programming-hero.com/api/category/${id}`;
   fetch(url)
     .then((res) => res.json())
-    .then((data) => displyInnrCat(data.plants));
+    .then((data) => {
+      displyInnrCat(data.plants);
+      hideLoader();
+    });
 };
 
 const displyInnrCat = (InnrCats) => {
-  // console.log(InnrCats);
   const categoriesContainer = document.getElementById("allplants");
   categoriesContainer.innerHTML = "";
 
   for (let InnrCat of InnrCats) {
-    // console.log(InnrCat);
     const categoriCard = document.createElement("div");
-    categoriCard.innerHTML = `
-      <div class="bg-white h-full rounded-xl shadow  hover:shadow-lg transition">
-                                <div class=" bg-gray-200 rounded flex items-center justify-center">
-                                    <img loading="lazy" class="w-full h-64 object-cover rounded-t-lg" src="${InnrCat.image}" alt="">
-                                </div>
-                                <button class="font-semibold px-3 mt-2">${InnrCat.name}</button>
+    categoriCard.classList.add(
+      "bg-white",
+      "h-full",
+      "rounded-xl",
+      "shadow",
+      "hover:shadow-lg",
+      "transition"
+    );
 
-                                <p class="text-sm p-3 text-gray-600">${InnrCat.description}</p>
-                                <div class="flex justify-between items-center px-3 mt-2">
-                                    <span
-                                        class="text-xs  px-3 py-1 bg-green-100 text-green-700 rounded">${InnrCat.category}</span>
-                                    <span class="font-semibold">${InnrCat.price}</span>
-                                </div>
-                                <div class="p-3">
-                                    <button onclick="cartBtn"
-                                        class="w-full mt-3 mx-auto py-2 rounded-full bg-green-600 text-white hover:bg-green-700">
-                                        Add to Cart</button>
-                                </div>
-                            </div>
+    categoriCard.innerHTML = `
+      <div class="bg-gray-200 rounded flex items-center justify-center">
+        <img loading="lazy" class="w-full h-64 object-cover rounded-t-lg" src="${InnrCat.image}" alt="">
+      </div>
+      <button class="modalBtn font-semibold px-3 mt-2 cursor-pointer">${InnrCat.name}</button>
+      <p class="text-sm p-3 text-gray-600">${InnrCat.description}</p>
+      <div class="flex justify-between items-center px-3 mt-2">
+        <span class="text-xs px-3 py-1 bg-green-100 text-green-700 rounded">${InnrCat.category}</span>
+        <span class="font-semibold">৳${InnrCat.price}</span>
+      </div>
+      <div class="p-3">
+        <button class="addBtn w-full mt-3 mx-auto py-2 rounded-full bg-green-600 text-white hover:bg-green-700 cursor-pointer">
+          Add to Cart
+        </button>
+      </div>
     `;
+
+    const modalBtn = categoriCard.querySelector(".modalBtn");
+    modalBtn.addEventListener("click", () => {
+      showModal(InnrCat);
+    });
+
+    const addBtn = categoriCard.querySelector(".addBtn");
+    addBtn.addEventListener("click", () => {
+      alert(`${InnrCat.name} has been added to the cart`);
+      const cartDisplay = document.getElementById("cartsBtn");
+      const cartContainer = document.createElement("div");
+      cartContainer.classList.add(
+        "flex",
+        "items-center",
+        "justify-between",
+        "p-2"
+      );
+
+      cartContainer.innerHTML = `
+        <div>
+          <h2 class="font-semibold">${InnrCat.name}</h2>
+          <p class="text-sm text-gray-600">৳${InnrCat.price}</p>
+        </div>
+        <div>
+          <i class="fa-solid fa-x cursor-pointer text-red-500 removeBtn"></i>
+        </div>
+      `;
+
+      cartDisplay.appendChild(cartContainer);
+
+      total += parseFloat(InnrCat.price);
+      document.getElementById("totalPrice").innerText = `৳${total}`;
+
+      const removeBtn = cartContainer.querySelector(".removeBtn");
+      removeBtn.addEventListener("click", () => {
+        cartContainer.remove();
+        total -= parseFloat(InnrCat.price);
+        document.getElementById("totalPrice").innerText = `৳${total}`;
+      });
+    });
+
     categoriesContainer.appendChild(categoriCard);
   }
 };
 
-let total = 0;
+const showModal = (plant) => {
+  let modal = document.getElementById("plantModal");
+  if (!modal) {
+    modal = document.createElement("div");
+    modal.id = "plantModal";
+    modal.classList.add(
+      "fixed",
+      "inset-0",
+      "flex",
+      "items-center",
+      "justify-center",
+      "bg-white/30",
+      "backdrop-blur-[2px]",
+      "bg-opacity-5",
+      "z-50"
+    );
+    modal.innerHTML = `
+      <div class="bg-white rounded-lg w-11/12 max-w-lg p-4 relative">
+         <h2 id="modalName" class="text-xl font-semibold my-4"></h2>
+        <img id="modalImage" class="w-full h-64 object-cover rounded" src="" alt="">
+       
+        <p class="text-sm text-gray-600 my-5" id="modalDesc"></p>
+        <div class="flex justify-between mt-2">
+          <span class="text-xs px-2 py-1 bg-green-100 mb-16 rounded" id="modalCategory"></span>
+          <span class="font-semibold " id="modalPrice"></span>
+        </div>
+        <button id="closeModal" class="btn absolute bottom-2 right-2 text-gray-700 font-bold text-lg">close</button>
+      </div>
+    `;
+    document.body.appendChild(modal);
+
+    modal.querySelector("#closeModal").addEventListener("click", () => {
+      modal.classList.add("hidden");
+    });
+  }
+
+  modal.querySelector("#modalImage").src = plant.image;
+  modal.querySelector("#modalName").textContent = plant.name;
+  modal.querySelector("#modalDesc").textContent = plant.description;
+  modal.querySelector("#modalCategory").textContent = plant.category;
+  modal.querySelector("#modalPrice").textContent = `Price:৳ ${plant.price}`;
+  console.log(plant.price);
+  modal.classList.remove("hidden");
+};
 
 const plantsData = () => {
+  showLoader();
   fetch("https://openapi.programming-hero.com/api/plants")
     .then((res) => res.json())
-    .then((data) => allPlantsDisply(data.plants));
+    .then((data) => {
+      allPlantsDisply(data.plants);
+      hideLoader();
+    });
 };
 
 const allPlantsDisply = (allPlants) => {
@@ -76,28 +370,40 @@ const allPlantsDisply = (allPlants) => {
 
   for (let allPlant of allPlants) {
     const plantsCard = document.createElement("div");
+    plantsCard.classList.add(
+      "bg-white",
+      "h-full",
+      "rounded-xl",
+      "shadow",
+      "hover:shadow-lg",
+      "transition"
+    );
+
     plantsCard.innerHTML = `
-      <div class="bg-white h-full rounded-xl shadow hover:shadow-lg transition">
-        <div class="bg-gray-200 rounded flex items-center justify-center">
-          <img loading="lazy" class="w-full h-64 object-cover rounded-t-lg" src="${allPlant.image}" alt="">
-        </div>
-        <button class="openModal font-semibold px-3 mt-2">${allPlant.name}</button>
-        <p class="text-sm p-3 text-gray-600">${allPlant.description}</p>
-        <div class="flex justify-between items-center px-3 mt-2">
-          <span class="text-xs px-3 py-1 bg-green-100 text-green-700 rounded">${allPlant.category}</span>
-          <span class="font-semibold">৳${allPlant.price}</span>
-        </div>
-        <div class="p-3">
-          <button
-            class="addBtn w-full mt-3 mx-auto py-2 rounded-full bg-green-600 text-white hover:bg-green-700">
-            Add to Cart
-          </button>
-        </div>
+      <div class="bg-gray-200 rounded flex items-center justify-center">
+        <img loading="lazy" class="w-full h-64 object-cover rounded-t-lg" src="${allPlant.image}" alt="">
+      </div>
+      <button class="modalBtn font-semibold px-3 mt-2 cursor-pointer">${allPlant.name}</button>
+      <p class="text-sm p-3 text-gray-600">${allPlant.description}</p>
+      <div class="flex justify-between items-center px-3 mt-2">
+        <span class="text-xs px-3 py-1 bg-green-100 text-green-700 rounded">${allPlant.category}</span>
+        <span class="font-semibold">৳${allPlant.price}</span>
+      </div>
+      <div class="p-3">
+        <button class="addBtn w-full mt-3 mx-auto py-2 rounded-full bg-green-600 text-white hover:bg-green-700 cursor-pointer">
+          Add to Cart
+        </button>
       </div>
     `;
 
+    const modalBtn = plantsCard.querySelector(".modalBtn");
+    modalBtn.addEventListener("click", () => {
+      showModal(allPlant);
+    });
+
     const addBtn = plantsCard.querySelector(".addBtn");
     addBtn.addEventListener("click", () => {
+      alert(`${allPlant.name} has been added to the cart`);
       const cartDisplay = document.getElementById("cartsBtn");
       const cartContainer = document.createElement("div");
       cartContainer.classList.add(
@@ -120,14 +426,13 @@ const allPlantsDisply = (allPlants) => {
       cartDisplay.appendChild(cartContainer);
 
       total += parseFloat(allPlant.price);
-      document.getElementById("totalPrice").innerText = total;
+      document.getElementById("totalPrice").innerText = `৳${total}`;
 
       const removeBtn = cartContainer.querySelector(".removeBtn");
       removeBtn.addEventListener("click", () => {
         cartContainer.remove();
-
         total -= parseFloat(allPlant.price);
-        document.getElementById("totalPrice").innerText = total;
+        document.getElementById("totalPrice").innerText = `৳${total}`;
       });
     });
 
